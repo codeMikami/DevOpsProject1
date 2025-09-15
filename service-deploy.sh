da2121ea0efea09f1e0bb0cdeee5b4497a86c275
Reikon <mikamithejudgement@gmail.com>
2025-09-15 12:15:22 +0000

    Deploy set up

--- service-deploy.sh
+++ service-deploy.sh
@@ -32,8 +32,8 @@ fi
 
 if ! command -v docker-compose &> /dev/null && ! docker compose version &> /dev/null; then
     echo "Installing Docker Compose..."
-    curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
+    curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+    chmod +x /usr/bin/docker-compose
 else
     echo "Docker Compose is already installed"
 fi
